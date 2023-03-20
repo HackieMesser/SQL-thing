c899f61662f88bf9eb39c41582b6373386a98de7
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-03-20 21:10:41 +0100

    Update advanced.sql

--- advanced.sql
+++ advanced.sql
@@ -49,3 +49,7 @@ alt way
 select '('|| lat||', '|| lon||')' as "concat location"   FROM tutorial.sf_crime_incidents_2014_01 
 date concat
 select substr("date",7,4)||'-'||left("date",2)||'-' ||substr("date",4,2) as new_date FROM tutorial.sf_crime_incidents_2014_01 
+
+SELECT left(category,1)||right(lower(category),length(category)-1)
+
+  FROM tutorial.sf_crime_incidents_2014_01
