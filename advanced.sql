2b47266b25956d6bd1b1cbc8017a94ebb2996832
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-04-01 14:41:20 +0200

    Update advanced.sql

--- advanced.sql
+++ advanced.sql
@@ -64,3 +64,14 @@ SELECT
 select date_trunc('week', cleaned_date)::date as week, count(incidnt_num)  from  tutorial.sf_crime_incidents_cleandate group by 1 order by 1
 
 SELECT incidnt_num,now() at time zone 'PST' - "cleaned_date" as how_long_ago from  tutorial.sf_crime_incidents_cleandate
+
+
+select sub.* from (
+                select * from tutorial.sf_crime_incidents_2014_01 where category='WARRANTS' )
+      sub where sub.resolution='NONE' 
+
+
+
+      
+      
+   
